2972806c8d98267c32f3f27cca136d5741282a6c
Rajesh1285 <rajeshnuthalapati499@gmail.com>
2018-12-09 12:34:04 +0530

    add letter_opener

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users 
+  devise_for :users
   resources :homes
   root 'homes#index'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
